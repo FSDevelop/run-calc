2eea0960e58fc9fb65f589d2834355c1ca089112
Federico Sosa <fedesosa.nl@gmail.com>
2024-01-02 17:03:48 +0100

    add km to the day if cumulative

--- app.ts
+++ app.ts
@@ -2,47 +2,72 @@ import { Month, WeekDay } from './enums';
 
 // inputs
 let year = 2024;
-let month: Month = Month.Feb;
+let month: Month = Month.Jun;
 let restDays: WeekDay[] = [ WeekDay.Sun ];
-let targetKms = 300;
+let targetMonthDistance = 300;
 
 // long run config
 let longRunEnabled = true;
 let longRunDay: WeekDay = WeekDay.Sat;
-let longRunRate: number = 23; // % of the week
+let longRunRate: number = 25; // % of the week
 
 // formulas
-let totalDaysOfMonth = new Date(year, month + 1, 0).getDate();
-let trainingDays = 0;
+const totalDaysOfMonth = new Date(year, month + 1, 0).getDate();
 
 // formulate amount of training days within the month
-for (let day = 1; day <= totalDaysOfMonth; day++) {
-    // if current day is not rest
-    if (!restDays.includes(new Date(year, month, day).getDay())) {
+let trainingDays = 0;
+for (let day = 1; day <= totalDaysOfMonth; day++)
+    if (!restDays.includes(new Date(year, month, day).getDay()))
         trainingDays++;
-    }
-}
 
-// week calculations
 const weekTrainingDays = 7 - restDays.length;
-const weekDistance = targetKms / trainingDays * weekTrainingDays;
+const weekDistance = targetMonthDistance / trainingDays * weekTrainingDays;
 const longRunDistance = longRunEnabled ? Math.floor(weekDistance / 100 * longRunRate) : 0;
-const defaultDayDistance = Math.floor((weekDistance - longRunDistance) / (weekTrainingDays - 1));
+const longRunDistanceDecimal = longRunEnabled ? weekDistance / 100 * longRunRate : 0;
+const defaultDayDistance = Math.floor((weekDistance - longRunDistanceDecimal) / (weekTrainingDays - 1));
+const defaultDayDistanceDecimal = (weekDistance - longRunDistanceDecimal) / (weekTrainingDays - 1);
 
+let cumulativeDayDecimalDistanceLeft = 0;
+
+let totalFinalDistance = 0;
 // generate trainings
 for (let day = 1; day <= totalDaysOfMonth; day++) {
     const date = new Date(year, month, day);
-    let trainingType = 'easy run';
-    let dayDistance = defaultDayDistance;
+    let trainingType = 'Easy Run';
+    let dayDistance = 0;
+
+    if (cumulativeDayDecimalDistanceLeft >= 1) {
+        dayDistance += 1;
+        cumulativeDayDecimalDistanceLeft--;
+    }
 
     if (restDays.includes(date.getDay())) {
-        console.log('rest'); continue;
+        console.log(`${date.toDateString()} -`, 'Rest'); continue;
     }
 
     else if (longRunEnabled && date.getDay() === longRunDay) {
-        trainingType = 'long run';
-        dayDistance = longRunDistance;
+        trainingType = 'Long Run';
+        dayDistance += longRunDistance;
+        cumulativeDayDecimalDistanceLeft += longRunDistanceDecimal - longRunDistance;
+    }
+
+    else {
+        dayDistance += defaultDayDistance;
+        cumulativeDayDecimalDistanceLeft += defaultDayDistanceDecimal - defaultDayDistance;
+    }
+
+    totalFinalDistance+= dayDistance;
+
+    if ((day === totalDaysOfMonth && !restDays.includes(date.getDay())) || (day + 1 === totalDaysOfMonth && restDays.includes(new Date(year, month, day + 1).getDay()))) {
+        if (targetMonthDistance - totalFinalDistance > 0) {
+            dayDistance += targetMonthDistance - totalFinalDistance;
+            totalFinalDistance += targetMonthDistance - totalFinalDistance;
+        }
     }
 
     console.log(`${date.toDateString()} -`, `${dayDistance} km ${trainingType}`);
 }
+
+console.log();
+console.log('Total KM planned: ', totalFinalDistance);
+console.log('Total KM requested: ', targetMonthDistance);
